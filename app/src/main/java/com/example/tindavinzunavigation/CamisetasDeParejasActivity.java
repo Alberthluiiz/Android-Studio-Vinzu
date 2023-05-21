4df508f640abef1b880a995c90144703a0fedc52
Albertluiiz <luisguillen290301@gmail.com>
2023-05-20 22:56:23 -0500

    Inicio de proyecto Android

---
+++ app/src/main/java/com/example/tindavinzunavigation/CamisetasDeParejasActivity.java
@@ -0,0 +1,14 @@
+package com.example.tindavinzunavigation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CamisetasDeParejasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camisetas_de_parejas);
+    }
+}
